37748eb481ea259de3ccebe3b0335244503b3878
David Francisco <dfrancisco2677@gmail.com>
2022-05-31 14:58:58 -0700

    Finished renderCard and displayCharacters functions.

--- render-utils.js
+++ render-utils.js
@@ -1,8 +1,19 @@
+
+
+
 export function renderCard(character) {
+    
     const h2 = document.createElement('h2');
     const img = document.createElement('img');
-
+    const p = document.createElement('p');
+    const cardDiv = document.createElement('div');
     h2.textContent = character.name;
     img.src = character.image;
+    p.textContent = character.bio;
+    cardDiv.append(h2, img, p);
+    cardDiv.classList.add('card');
+
+    return cardDiv;
     //wait until we have more information
+
 }
