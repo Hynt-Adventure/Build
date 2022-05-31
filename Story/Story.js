d0e0168de98d5703a2a4c074088d03b72bbabe3d
Chad Stabler <stablerpsn@gmail.com>
2022-05-31 13:16:58 -0500

    added skeleton for all pages

---
+++ Story/Story.js
@@ -0,0 +1,11 @@
+const choice1 = document.getElementById('choice-1');
+const choice2 = document.getElementById('choice-2');
+
+choice1.addEventListener('click', () => {
+    location.replace('../END');
+});
+
+
+choice2.addEventListener('click', () => {
+    location.replace('../END');
+});
